ab44ea979e5c78e2ee2c988ac5abfdf18251f2cd
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2015-08-28 15:51:51 +0000

    Tweak stream state component

--- src/scripts/components/stream-status-view.jsx
+++ src/scripts/components/stream-status-view.jsx
@@ -15,13 +15,13 @@ export default class StreamStatusView extends React.Component {
 
         this.update = () => {
             this.setState({
-                readyState: eventManager.eventSource.readyState
+                readyState: eventManager.enabled ? eventManager.eventSource.readyState : -1
             })
         }
     }
 
     componentDidMount() {
-        this.interval = window.setInterval(this.update, 5000)
+        this.interval = window.setInterval(this.update, 2000)
         this.update()
     }
 
@@ -36,25 +36,36 @@ export default class StreamStatusView extends React.Component {
     render() {
         let style = {
             padding: '4px 8px',
-            marginRight: '10px',
-            color: Styles.Colors.blueGrey600,
-            backgroundColor: Styles.Colors.blueGrey50
+            marginRight: '10px'
         }
         let text = null
         switch (this.state.readyState) {
+            case -1:
+                text = 'not supported'
+                style.color = Styles.Colors.deepOrange500
+                style.backgroundColor = Styles.Colors.deepOrange50
+                break
             case 0:
                 text = 'connecting'
+                style.color = Styles.Colors.yellow800
+                style.backgroundColor = Styles.Colors.yellow50
                 break
             case 1:
-                text = 'open'
+                text = 'connected'
+                style.color = Styles.Colors.green700
+                style.backgroundColor = Styles.Colors.green50
                 break
             case 2:
                 text = 'closed'
+                style.color = Styles.Colors.red600
+                style.backgroundColor = Styles.Colors.red50
                 break
             default:
                 text = 'unknown'
+                style.color = Styles.Colors.grey600
+                style.backgroundColor = Styles.Colors.grey100
                 break
         }
-        return <span style={style}>{`Stream connection status: ${text}`}</span>
+        return <span style={style}>{`Stream: ${text}`}</span>
     }
 }
