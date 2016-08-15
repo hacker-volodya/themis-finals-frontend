ca14513fbf730ee22ca06ba4b4ee3037b22fb45a
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-08-15 16:56:00 +0000

    Update customize example

---
+++ customize.example.js
@@ -0,0 +1,10 @@
+import { Styles } from 'material-ui'
+
+export default {
+  contestTitle: 'TODO: Set up contest title',
+  primary1Color: Styles.Colors.indigoA700,
+  accent1Color: Styles.Colors.red500,
+  headerColor: Styles.Colors.white,
+  indexTitle: 'TODO: Set up title',
+  indexDescription: 'TODO: Set up description'
+}
