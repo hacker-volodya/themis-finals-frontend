18fa7b927878b3d5094958c920aa6c931f8a9c9b
Vladimir Lebedev <d3fl4t3@gmail.com>
2017-02-04 23:55:41 +0700

    customize.js changes

--- customize.example.js
+++ customize.example.js
@@ -1,10 +1,10 @@
 import { indigoA700, red500, white } from 'material-ui/styles/colors'
 
 export default {
-  contestTitle: 'TODO: Set up contest title',
-  primary1Color: indigoA700,
+  contestTitle: 'SiBears Training',
+  primary1Color: "#eb0c0c",
   accent1Color: red500,
   headerColor: white,
-  indexTitle: 'TODO: Set up title',
-  indexDescription: 'TODO: Set up description'
+  indexTitle: 'SiBears Classic CTF Training',
+  indexDescription: ''
 }
